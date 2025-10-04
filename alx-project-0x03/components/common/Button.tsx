e0039784c8667ee8874c05316f456920ddddf730
Wanjohi Wanjiru <wanjiruwanjohi@gmail.com>
2025-10-04 17:03:22 +0300

    separated interfaces frpm components

--- alx-project-0x03/components/common/Button.tsx
+++ alx-project-0x03/components/common/Button.tsx
@@ -2,14 +2,6 @@
 
 import { ButtonProps } from "@/interface";
 
-// interface ButtonProps {
-//   buttonLabel: string
-//   buttonSize?: string
-//   buttonBackgroundColor?: 'red' | 'blue' | 'orange' | 'green'
-//   action?: () => void
-// }
-
-
 const Button = ({ buttonLabel, buttonSize, buttonBackgroundColor, action }: ButtonProps) => {
 
   const backgroundColorClass = buttonBackgroundColor ? {
