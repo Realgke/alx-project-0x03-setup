9c673a10916eb71f2c1583289da41261b2524110
Wanjohi Wanjiru <wanjiruwanjohi@gmail.com>
2025-10-04 16:16:11 +0300

    kkkk

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,12 +1,12 @@
-import Button from "@/components/common/Button";
 import { useRouter } from "next/router";
+import Button from "@/components/common/Button";
 
 interface PageRouteProps {
   pageRoute: string
 }
 
 export default function Home() {
-  const router = useRouter()
+  const router = useRouter();
 
   // Imeperative routing with useRouter
   const routeToNextPage  = ({ pageRoute }: PageRouteProps) => {
